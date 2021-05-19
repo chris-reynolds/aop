165b6de959baa10a529afb44e7b3def55c569234
Chris Reynolds <chrisr@instantobjects.com>
2021-05-19 12:11:28 +1200

    Logger loads and saves to file. Also increase limit to 1000 and fix datestamp text bug

--- aopcommon/lib/src/Logger.dart
+++ aopcommon/lib/src/Logger.dart
@@ -2,6 +2,8 @@
 /// Created by Chris on 21/09/2018.
 ///
 import 'DateUtil.dart';
+import 'dart:io';
+
 
 typedef LoggerFunc = void Function(String s);
 
@@ -14,21 +16,35 @@ class _Log {
 
 // setup default loggers
   static LoggerFunc onMessage = (String s) {
-    String stampedMessage = '${formatDate(DateTime.now(),format: 'hh:mm:ss.lll')} $s';
+    String stampedMessage = '${formatDate(DateTime.now(),format: 'hh:nn:ss.lll')} $s';
     _logHistory.add(stampedMessage);
-    if (_logHistory.length > 100) _logHistory.removeAt(0);
+    if (_logHistory.length > 1000) _logHistory.removeAt(0);
  //   if (logLevel == eLogLevel.llMessage) onMessage(stampedMessage);
   };
 
   static LoggerFunc onError = (String s) => onMessage('--- ERROR ---- $s');
 
   List<String> get logHistory => _logHistory;
-  void clear() => _logHistory=[];
+  void clear() => _logHistory=<String>[];
   void message(String s) => onMessage(s);
 
   void error(String s) {
-    onError('${formatDate(DateTime.now(),format: 'hh:mm:ss.lll')} $s');
+    onError('${formatDate(DateTime.now(),format: 'hh:nn:ss.lll')} $s');
   }
+  String _logFilename;
+  set logFilename(String s) {_logFilename=s;}
+
+    Future<void> save() async  {
+      if (_logFilename==null) return;
+      IOSink fl = File(_logFilename).openWrite();
+      fl.writeAll(_logHistory,'\n');
+      await fl.close();
+    }
+    Future<void> load() async {
+      if (_logFilename==null) throw 'No log Filename';
+      _logHistory = await File(_logFilename).readAsLines();
+    }
+
 }  // of _log
 
 _Log log = _Log();
